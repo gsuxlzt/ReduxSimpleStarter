952c42c3983c3023ab988188e2ab6b8d165b0972
Jan Miguel Carangan <jami@Admins-Mac-mini.local>
2017-11-26 08:17:14 +0800

    :hevy_plus_sign: add flow dependency :wrench add and modify flowconfig.
    Signed-off-by: Jan Miguel Carangan <jami@Admins-Mac-mini.local>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
+// @flow
+
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAwndAZ0a58A9dWETWv4ySDlib9T8WSkDc';
-const App = () => <div>Hi!</div>;
+class App extends Component {
+  state = {
+    initial: true,
+  }
+  render() {
+    return (
+      <SearchBar />
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
