45f7304a542a60782e04dc52e6cbab7f78da3390
gsuxlzt <jami.carangan@gmail.com>
2017-11-29 18:04:23 +0800

    :hammer: add videoitems component. :construction: todo  make component dynamic.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
-const VideoList = props => (
-  <ul className="col-md-4 listgroup">
-    {props.videos.length}
-  </ul>
-);
+const VideoList = (props) => {
+  const VideoItems = props.videos.map(video => <VideoListItem key={video.etag} video={video} />);
+
+  return (
+    <ul className="col-md-4 list-group">
+      {VideoItems}
+    </ul>
+  );
+};
 
 export default VideoList;
