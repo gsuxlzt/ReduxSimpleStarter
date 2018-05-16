d94290f74fc3c193d89462eaf9adf01b6a82bb25
gsuxlzt <jami.carangan@gmail.com>
2018-05-16 10:42:42 +0800

    :recycle: refactor video_list component.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => {
-  const VideoItems = props.videos.map(video => <VideoListItem key={video.etag} video={video} />);
+const VideoList = ({ videos }) => {
+  const VideoItems = videos.map(video => <VideoListItem key={video.etag} video={video} />);
 
   return (
     <ul className="col-md-4 list-group">
