84581874e2ee7a4e90f5b55380347a8532b1795a
gsuxlzt <jami.carangan@gmail.com>
2018-05-16 12:00:03 +0800

    :sparkles: video detail component now working properly. :construction: still have to make it dynamic.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
-import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAwndAZ0a58A9dWETWv4ySDlib9T8WSkDc';
 
@@ -25,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
